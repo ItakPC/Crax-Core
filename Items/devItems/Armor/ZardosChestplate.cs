ad2fd3e20743dbf8b527d51e3e0aa8d75033643a
ItakPC <buildingstar.com@gmail.com>
2015-04-06 12:27:06 +0200

    Upcoming textures for tools.

--- Items/devItems/Armor/ZardosChestplate.cs
+++ Items/devItems/Armor/ZardosChestplate.cs
@@ -10,10 +10,30 @@ namespace CraxCore.Items.devItems.Armor
 {
     public class ZardosChestplate : ModItem
     {
+        public ZardosChestplate()
+        
+        {
+
+        }
+
         public override void Effects(Player p)
         {
+            p.lavaImmune = true;
+            p.findTreasure = true;
+            p.fireWalk = true;
+            p.armorStealth = true;
+            p.waterWalk = true;
+            p.blockRange += 10;
+            p.bulletDamage += 5f;
             p.maxMinions += 10;
+            p.statLifeMax2 += 1000;
         }
+
+        public override void DamageNPC(Player p, NPC n, int hitDir, ref int damage, ref float knockback, ref bool crit, ref float critMult)
+        {
+            p.statLife += 20;
+        }
+
         public override void ArmorSetBonus(Player player) 
         {
             player.maxMinions += 10;
